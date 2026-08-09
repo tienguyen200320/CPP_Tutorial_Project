fbeccddc77e985d2b55b986964e87f562f22403c
tienguyen200320 <nguyenviettien20032000@gmail.com>
2026-08-09 20:06:34 +0700

    commit 2

--- build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/DELL/tien/Desktop/GGTest/googletest
-# Build directory: C:/Users/DELL/tien/Desktop/Software_Learning/ProjectCPP/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/DELL/tien/Desktop/GGTest/googletest/googlemock
-# Build directory: C:/Users/DELL/tien/Desktop/Software_Learning/ProjectCPP/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
